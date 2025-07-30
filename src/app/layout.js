aac8984f21e8480a3aeae6bd78981deab42e56e2
Abinson Suresh <88340846+Abinsonsuresh@users.noreply.github.com>
2025-07-30 12:41:02 +0530

    fix: providers added to layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,20 +1,21 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "../styles/globals.css";
+import { Geist, Geist_Mono } from "next/font/google"
+import "../styles/globals.css"
+import Providers from "./providers"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
-});
+})
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
-});
+})
 
 export const metadata = {
   title: "Next Js Template",
   description: "Project description",
-};
+}
 
 export default function RootLayout({ children }) {
   return (
@@ -22,8 +23,8 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Providers>{children}</Providers>
       </body>
     </html>
-  );
+  )
 }
